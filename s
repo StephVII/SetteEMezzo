262aaccf8da9ec8bb775e8cb4032aa209c6c8a77
Steph Trovato <trovato.stefano04@gmail.com>
2023-11-15 12:36:39 +0100

    Commit 1: Generazione Mazzo 2.0

---
+++ s
@@ -0,0 +1,5 @@
+[33mcommit 774e114cf10b06f40f0ed10defa7968da8728aa2[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Steph Trovato <trovato.stefano04@gmail.com>
+Date:   Wed Nov 15 12:05:52 2023 +0100
+
+    Commit 1: Generazione Mazzo
--- src/org/generation/italy/Main.java
+++ src/org/generation/italy/Main.java
@@ -1,10 +1,46 @@
 package org.generation.italy;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		Random R = new Random();
+		
+		String risposta, cartaEstratta;
+		int numCarta, i, j;
+		
+		ArrayList<String> mazzo = new ArrayList<>();
+		String[] semi = {"Denari", "Coppe", "Spade", "Bastoni"};
+		
+		for(j=0; j<4; j++)
+		{
+			for(i=1; i<=10; i++)
+			{
+				mazzo.add(i+" di "+semi[j]);
+			}
+		}
+		
+		for(i=0; i<40; i++)
+		{
+			System.out.println(mazzo.get(i));
+		}
+		
+		/*System.out.println("Vuoi estrarre una carta? (si/no)");
+		risposta = sc.nextLine();
+		if(risposta.equals("si"))
+		{
+			numCarta = R.nextInt(mazzo.length);
+			cartaEstratta = mazzo[numCarta];
+			System.out.println("La tua carta è "+cartaEstratta);
+		}*/
+		
+		
 	}
 
 }
